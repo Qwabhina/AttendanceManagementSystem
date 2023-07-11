e52d7a0cb483b16891e55993471912796cee9261
Qwabhina <ben.macfinn@gmail.com>
2023-07-11 21:55:58 +0000

    Added a new System
    A whole system rewrite with another project.

--- index.php
+++ index.php
@@ -1,108 +1,167 @@
-<?php
-
-if(isset($_POST['login']))
-{
-
-
-	try{
-
-
-		if(empty($_POST['username'])){
-			throw new Exception("Username is required!");
-			
-		}
-		if(empty($_POST['password'])){
-			throw new Exception("Password is required!");
-			
-		}
-
-		 include ('connect.php');
-
-		$row=0;
-		$result=mysqli_query($conn,"select * from admininfo where username='$_POST[username]' and password='$_POST[password]' and type='$_POST[type]'");
-
-		$row=mysqli_num_rows($result);
-
-		if($row>0 && $_POST["type"] == 'teacher'){
-			session_start();
-			$_SESSION['name']="oasis";
-			header('location: teacher/index.php');
-		}
-
-		else if($row>0 &&  $_POST["type"] == 'student'){
-			session_start();
-			$_SESSION['name']="oasis";
-			header('location: student/index.php');
-		}
-
-		else if($row>0 && $_POST["type"] == 'admin'){
-			session_start();
-			$_SESSION['name']="oasis";
-			header('location: admin/index.php');
-		}
-
-		else{
-			throw new Exception("Invalid Username, Password or Role. Try Again!!!");
-
-		}
-	}
-
-
-	catch(Exception $e){
-		$error_msg=$e->getMessage();
-	}
-
-}
-
+<?php 
+include 'Includes/dbcon.php';
+session_start();
 ?>
 
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Attendance Management System</title>
-    <link rel="stylesheet" href="CSS/style.css">
-  </head>
-  <body>
-    <div class="textbox">
-      <h1>Attendance Management System</h1>
-    </div>
-  
-    <div class="center">
-      <h1>Login</h1>
-	  <?php
+<html lang="en">
 
-if(isset($error_msg))
-{
-	echo $error_msg;
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="img/logo/attnlg.jpg" rel="icon">
+    <title>Attendance Management System - Login</title>
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/ruang-admin.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-login" style="background-image: url('img/logo/loral1.jpe00g');">
+    <!-- Login Content -->
+    <div class="container-login">
+        <div class="row justify-content-center">
+            <div class="col-xl-10 col-lg-12 col-md-9">
+                <div class="card shadow-sm my-5">
+                    <div class="card-body p-0">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="login-form">
+                                    <h5 align="center">STUDENT ATTENDANCE SYSTEM</h5>
+                                    <div class="text-center">
+                                        <img src="img/logo/attnlg.jpg" style="width:100px;height:100px">
+                                        <br><br>
+                                        <h1 class="h4 text-gray-900 mb-4">Login Panel</h1>
+                                    </div>
+                                    <form class="user" method="Post" action="">
+                                        <div class="form-group">
+                                            <select required name="userType" class="form-control mb-3">
+                                                <option value="">--Select User Roles--</option>
+                                                <option value="Administrator">Administrator</option>
+                                                <option value="ClassTeacher">ClassTeacher</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" required name="username" id="exampleInputEmail" placeholder="Enter Email Address">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" name="password" required class="form-control" id="exampleInputPassword" placeholder="Enter Password">
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="custom-control custom-checkbox small" style="line-height: 1.5rem;">
+                                                <input type="checkbox" class="custom-control-input" id="customCheck">
+                                                <!-- <label class="custom-control-label" for="customCheck">Remember
+                          Me</label> -->
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="submit" class="btn btn-success btn-block" value="Login" name="login" />
+                                        </div>
+                                    </form>
+
+                                    <?php
+
+  if(isset($_POST['login'])){
+
+    $userType = $_POST['userType'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $password = md5($password);
+
+    if($userType == "Administrator"){
+
+      $query = "SELECT * FROM tbladmin WHERE emailAddress = '$username' AND password = '$password'";
+      $rs = $conn->query($query);
+      $num = $rs->num_rows;
+      $rows = $rs->fetch_assoc();
+
+      if($num > 0){
+
+        $_SESSION['userId'] = $rows['Id'];
+        $_SESSION['firstName'] = $rows['firstName'];
+        $_SESSION['lastName'] = $rows['lastName'];
+        $_SESSION['emailAddress'] = $rows['emailAddress'];
+
+        echo "<script type = \"text/javascript\">
+        window.location = (\"Admin/index.php\")
+        </script>";
+      }
+
+      else{
+
+        echo "<div class='alert alert-danger' role='alert'>
+        Invalid Username/Password!
+        </div>";
+
+      }
+    }
+    else if($userType == "ClassTeacher"){
+
+      $query = "SELECT * FROM tblclassteacher WHERE emailAddress = '$username' AND password = '$password'";
+      $rs = $conn->query($query);
+      $num = $rs->num_rows;
+      $rows = $rs->fetch_assoc();
+
+      if($num > 0){
+
+        $_SESSION['userId'] = $rows['Id'];
+        $_SESSION['firstName'] = $rows['firstName'];
+        $_SESSION['lastName'] = $rows['lastName'];
+        $_SESSION['emailAddress'] = $rows['emailAddress'];
+        $_SESSION['classId'] = $rows['classId'];
+        $_SESSION['classArmId'] = $rows['classArmId'];
+
+        echo "<script type = \"text/javascript\">
+        window.location = (\"ClassTeacher/index.php\")
+        </script>";
+      }
+
+      else{
+
+        echo "<div class='alert alert-danger' role='alert'>
+        Invalid Username/Password!
+        </div>";
+
+      }
+    }
+    else{
+
+        echo "<div class='alert alert-danger' role='alert'>
+        Invalid Username/Password!
+        </div>";
+
+    }
 }
 ?>
-      <form method="post">
-        <div class="txt_field">
-          <input type="text" name="username" id="input1" required>
-          <span></span>
-          <label for="input1">Username</label>
-        </div>
-        <div class="txt_field">
-          <input type="password" name="password" id="input1" required>
-          <span></span>
-          <label for="input1">Password</label>
-        </div>
-        <div class="pass"><a href="reset.html">Forgot Password?</a></div>
-        <div class="Role">
-          <label for="input1">Role:</label>
-          <div class="check_box">
-            <label> <input type="radio" name="type" value="student"> Student </label>
-            <label><input type="radio" name="type" value="teacher"> Teacher </label>
-            <label><input type="radio" name="type"value="admin"> Admin </label>
-          </div>
-          </div>
-        <input type="submit" value="Login" name="login">
-        <div class="signup_link">
-          Not a member? <a href="#">Contact admin</a>
-        </div>
-      </form>
-    </div>
 
-  </body>
+                                    <!-- <hr>
+                    <a href="index.html" class="btn btn-google btn-block">
+                      <i class="fab fa-google fa-fw"></i> Login with Google
+                    </a>
+                    <a href="index.html" class="btn btn-facebook btn-block">
+                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                    </a> -->
+
+
+                                    <div class="text-center">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Login Content -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="js/ruang-admin.min.js"></script>
+</body>
+
 </html>
